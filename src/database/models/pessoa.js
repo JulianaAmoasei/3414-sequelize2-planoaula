2699b054d71ea47ce719a6e000468491d1aab9ed
JulianaAmoasei <late.parrot@gmail.com>
2023-09-21 11:09:15 -0300

    videos 2.3 e 2.4: validações

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,3 +1,5 @@
+const isCpfValido = require('../../helpers/validadorCpf.js');
+
 'use strict';
 const {
   Model
@@ -16,9 +18,32 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoa.init({
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cpf: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: 'o campo deve ter no mínimo 3 caracteres'
+        }
+      }
+    },
+    email: { 
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'formato do email inválido'
+        }
+      }
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf) => {
+          if (!isCpfValido(cpf)) throw new Error('número de CPF inválido');
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
